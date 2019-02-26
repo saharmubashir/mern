cf9496275b73ba9b0290255974b1a4ca62692a52
saharmubashir <46933709+saharmubashir@users.noreply.github.com>
2019-02-26 14:06:13 +0500

    Add files via upload

---
+++ queue and stack.js
@@ -0,0 +1,65 @@
+var stack=[];
+
+load();
+
+function load(){
+  stack = JSON.parse(localStorage.values);
+  
+queue=JSON.parse(localStorage.queueData);
+
+}
+  
+function insertElementInStack(){
+  
+stack = JSON.parse(localStorage.values);
+ 
+ var insertedValue=prompt("Enter the value to be inserted in stack");
+ 
+ stack.push(insertedValue);
+  alert("Value inserted!");
+ 
+ localStorage.values = JSON.stringify(stack);
+
+
+}
+
+function removeElementFromStack(){
+ 
+ stack.pop();
+  localStorage.values = JSON.stringify(stack);
+ 
+
+ stack = JSON.parse(localStorage.values);
+
+}
+
+var queue=[];
+
+function insertElementInQueue()
+{
+  
+queue=JSON.parse(localStorage.queueData);
+
+var queueValues=prompt("Enter the value to be inserted in queue");
+
+queue.push(queueValues);
+ 
+alert("Value inserted!");
+ 
+ localStorage.queueData=JSON.stringify(queue);
+
+
+
+}
+
+
+function removeElementFromStack(){
+  
+queue.shift();
+  
+localStorage.queueData=JSON.stringify(queue);
+
+
+
+
+}
