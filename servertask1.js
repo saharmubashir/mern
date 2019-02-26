a9b6f5275c17416bc3f36c5a32c6c8c9836efa13
saharmubashir <46933709+saharmubashir@users.noreply.github.com>
2019-02-26 14:18:12 +0500

    Add files via upload

---
+++ servertask1.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express()
+
+var path = require('path');
+
+
+
+app.get('/',(request,response)  =>  {
+    
+response.send("hello world");
+
+})
+
+app.get('/batman',(request,response)=> {
+    
+response.send("i am batman !!")
+})
+
+app.get('/spiderman',(request,response)=> {
+    
+response.send("<h1>i am spider man</h1>")
+})
+
+app.get('/runhtml',(request,response)=> {
+   
+response.sendFile(path.join(__dirname, '../public', 'index.html'))
+});
+
+
+app.listen(3000);
