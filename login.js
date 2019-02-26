b4783ff330480adf9b22757de20a64c48c6b84dd
saharmubashir <46933709+saharmubashir@users.noreply.github.com>
2019-02-26 14:22:56 +0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+function verify(){
+    let name = document.getElementById("un").value;
+    
+let password=document.getElementById("pw").value;
+    
+if(name==="isbah"password==="password");
+    
+window.open('welcome.html');
+}
