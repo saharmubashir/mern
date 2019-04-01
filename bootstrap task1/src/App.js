631de18c352955459b69e722cf8b30eca38e449b
saharmubashir <46933709+saharmubashir@users.noreply.github.com>
2019-04-01 14:09:37 +0500

    Add files via upload

---
+++ bootstrap task1/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="rounded-circle" >
+     <img src="https://raw.githubusercontent.com/BlackrockDigital/startbootstrap-resume/master/img/profile.jpg" alt="xyz" className="rounded-circle"></img>
+   
+   </div> )
+  }
+}
+
+export default App;
